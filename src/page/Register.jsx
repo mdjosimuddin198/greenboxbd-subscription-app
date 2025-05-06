75f604d52a7175abd1a24c5680c2ac0a0e49056f
josimuddin198 <josimweb0504@gmail.com>
2025-05-06 09:57:15 +0600

    added vaildation for password

--- src/page/Register.jsx
+++ src/page/Register.jsx
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router";
 
 const Register = () => {
+  const [errorMsg, setErrorMsg] = useState("");
+  const [successMsg, setSuccessMsg] = useState(false);
   const handleRegister = (e) => {
     e.preventDefault();
+    setErrorMsg("");
+    setSuccessMsg(false);
     const form = e.target;
-    const name = e.target.name.value;
-    console.log(form, name);
+    const name = form.name.value;
+    const photo = form.photo.value;
+    const password = form.password.value;
+    const email = form.email.value;
+
+    if (password.length < 6) {
+      setErrorMsg("passworb must be 6 characters or longer ");
+    } else if (!/[A-Z]/.test(password)) {
+      setErrorMsg("Password must contain at least one uppercase letter.");
+    } else if (!/[a-z]/.test(password)) {
+      setErrorMsg(" Password must contain at least one lowercase letter.");
+    } else {
+      setSuccessMsg(true);
+    }
+
+    console.log(name, photo, password, email);
+    return;
   };
   return (
     <>
@@ -20,18 +39,37 @@ const Register = () => {
               name="name"
               className="input"
               placeholder="Name"
+              required
             />
             {/* photourl  */}
             <label className="label text-xl">PhotoURL</label>
-            <input type="text" className="input" placeholder="PhotoURL" />
+            <input
+              type="text"
+              name="photo"
+              className="input"
+              placeholder="PhotoURL"
+              required
+            />
             {/* email */}
             <label className="label text-xl">Email</label>
-            <input type="email" className="input" placeholder="Email" />
+            <input
+              type="email"
+              name="email"
+              className="input"
+              placeholder="Email"
+              required
+            />
             {/* password */}
             <label className="label text-xl">Password</label>
-            <input type="password" className="input" placeholder="Password" />
+            <input
+              type="password"
+              name="password"
+              className="input"
+              placeholder="Password"
+              required
+            />
             <div></div>
-            <button className="btn btn-neutral mt-4">Login</button>
+            <button className="btn btn-neutral mt-4">Register Now</button>
             <p className="text-sm text-base-200">
               Allready have account{" "}
               <Link className="text-blue-400 underline" to="/auth/login">
@@ -39,6 +77,10 @@ const Register = () => {
                 Login Now{" "}
               </Link>
             </p>
+            {errorMsg && <p className="text-red-400">{errorMsg}</p>}
+            {successMsg && (
+              <p className="text-green-500">Account have Create SuccessFully</p>
+            )}
           </form>
         </div>
       </div>
